4a9c292c0884df2f17df8940e61ad8367eba48d9
Shreedevi410 <shreedevi.v.nayak@gmail.com>
2026-09-02 17:32:31 +0530

    celcius

---
+++ JavaScript/celciusToFharan.js
@@ -0,0 +1,8 @@
+function celsiusToFahrenheit(celsius) {
+    // find the conversion from Celsius to
+    // Fahrenheit Fahrenheit=(Celsius×9/5)+32
+    return (celsius * 9 / 5) + 32;
+
+}
+
+console.log(celsiusToFahrenheit(20));
