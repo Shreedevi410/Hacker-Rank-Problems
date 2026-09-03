dac3fd8ece42a400260f5695939d571ae2d36714
Shreedevi410 <shreedevi.v.nayak@gmail.com>
2026-09-03 20:38:26 +0530

    code

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -14,3 +14,18 @@ a = 3;
 console.log("Sync:", a);
 
 //microtask and macro task
+
+//functional block
+function outer() {
+    let outerVar = "I'm in the outer scope!";
+
+    function inner() {
+        console.log(outerVar);
+    }
+    return inner;
+}
+
+const closure = outer();
+
+closure();
+closure();
