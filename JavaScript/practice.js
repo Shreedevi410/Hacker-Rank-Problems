1aef8afa318b6dc92a24114cae462e7bf653a861
Shreedevi410 <shreedevi.v.nayak@gmail.com>
2026-09-03 20:40:33 +0530

    code

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -29,3 +29,22 @@ const closure = outer();
 
 closure();
 closure();
+
+//
+function Person(name) {
+    this.name = name;
+    
+    this.sayName = function () {
+        console.log(this.name);
+    };
+
+    setTimeout(function () {
+        console.log(this.name); 
+        // In non-strict mode, this refers to the global object.
+        // In strict mode, this is undefined.
+    }.bind(this), 1000); 
+    // Fix with bind
+}
+
+const G = new Person("GFG");
+G.sayName();
