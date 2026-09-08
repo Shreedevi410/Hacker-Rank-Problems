db267a2f0fdcaf55eda3e4ea22f2f5a70c16836c
Shreedevi410 <shreedevi.v.nayak@gmail.com>
2026-09-08 19:51:36 +0530

    binary search

--- Java/search.java
+++ Java/search.java
@@ -21,4 +21,21 @@ Public class Demo {
         }
         return -1
     }
+
+        public static int linearSearch(int[] nums, int target){
+        int left = 0;
+        int right = nums.length -1;
+
+        While(left <= right){
+            int mid = (left + right)/2;
+            if (num[mid] == target){
+                return mid;
+            } else if (num[mid] < target) {
+                left = mid +1
+            } else {
+                right = mid -1
+            }
+        }
+        return -1
+    }
 }
